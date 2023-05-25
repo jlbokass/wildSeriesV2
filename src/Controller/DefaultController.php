71594a3ffc941a599266d10793e696da392699eb
jlbokass <jean-le-grand.bokassa@wildcodeschool.com>
2023-05-25 16:16:09 +0200

    created default page controller and template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * Homepage of wild series
+     *
+     * @return Response
+     */
+    #[Route('/', name: 'app_default')]
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig');
+    }
+}
